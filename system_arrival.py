809c1da23bdddd4cad84f74f5f99833cfec6c195
peteramazonian <peteramazonian@gmail.com>
2020-06-03 18:56:10 +0430

    class SystemArrival and function departure. works well. maybe departure needs to be upgraded

--- system_arrival.py
+++ system_arrival.py
@@ -1,6 +1,5 @@
-from time_generator import TimeGenerator
-from time_management import add_to_fel, clock
-import random
+import time_management
+from time_management import add_to_fel
 
 __id__ = 10000
 
@@ -11,36 +10,15 @@ def id_generator():
     return __id__
 
 
-class NumberGenerator():
-    # TODO define number generator
-    class Static(random.Random):
-        pass
-
-    class Poisson(random.Random):
-        pass
-
-    class Discrete(random.Random):
-        def __init__(self, x: tuple, fx: tuple, *args):
-            self.fx_list = fx
-            self.x_list = x
-            if len(self.x_list) != len(self.fx_list):
-                raise ValueError("x_list and fx_list should have same number of elements")
-            super().__init__(*args)
-
-        def generate(self):
-            rnd = self.random()
-            print (rnd)
-            for i in range(self.fx_list.__len__()):
-                if rnd < sum(self.fx_list[:i + 1]):
-                    return self.x_list[i]
-
-
-
 class SystemArrival:
+    list = []
+
     def __init__(self, name, inter_arrival_time_generator, number_of_arrivals_generator):
         self.name = name
         self.time_generator = inter_arrival_time_generator
         self.number_generator = number_of_arrivals_generator
+        SystemArrival.list.append(self)
+        self.m_list = __import__('movement').Movement.list
 
     # Overriding Python's original __repr__ function
     def __repr__(self):
@@ -48,8 +26,12 @@ class SystemArrival:
 
     def new_arrival(self, number_of_arrivals):
         for i in range(number_of_arrivals):
-            # TODO define movement
-            movement[0](id_generator())
+            self.m_list[0].move(id_generator())
         # generating next arrival event
-        event_notice = (self.time_generator.generate() + clock, self.name, self.number_generator.generate(), self.new_arrival)
+        event_notice = (
+            self.time_generator.generate() + time_management.clock, self.name, self.number_generator.generate(), self.new_arrival)
         add_to_fel(event_notice)
+
+
+def departure(costumer_id):
+    print("%s   Departured at   %s" % (costumer_id, time_management.clock))
